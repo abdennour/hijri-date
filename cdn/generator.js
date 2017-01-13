966c9604f267e6914746cbef039391335d25385b
abdennour <mail@abdennoor.com>
2017-01-13 06:18:45 +0300

    minified release in /cdn folder

--- cdn/generator.js
+++ cdn/generator.js
@@ -1,6 +1,14 @@
 const rt = process.cwd();
-const {version} = require(rt+'/package.json');
+const {version, name} = require(rt+'/package.json');
 const shell = require('child_process').execSync;
-const cmd = (suffix) =>`node ${rt}/node_modules/browserify/bin/cmd ${rt}/index.js -o ${rt}/cdn/hijri-date-${suffix}.js` ;
-shell(cmd(version));
-shell(cmd('latest'));
+const cmds ={
+ browserify:(suffix =>`node ${rt}/node_modules/browserify/bin/cmd ${rt}/index.js -o ${rt}/cdn/${name}-${suffix}.js`) ,
+ minify : (suffix => `node node_modules/uglify-js/bin/uglifyjs cdn/${name}-${suffix}.js -o cdn/${name}-${suffix}.min.js`)
+};
+
+
+[version, 'latest'].forEach(r =>
+  Object.keys(cmds).forEach(cmd =>
+    shell(cmds[cmd](r))
+  )
+);
